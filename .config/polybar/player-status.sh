0b830c00ba64ea4020350e63e4d03cb1c9db3d80
akhil3417 <akhilpratapsing3417@gmail.com>
2023-01-27 11:00:15 +0530

    trivial

--- .config/polybar/player-status.sh
+++ .config/polybar/player-status.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# [[file:../doom/exwmmain.org::*polybar config][polybar config:2]]
 status="$(playerctl -p spotify status 2>&1)"
 if [ "$status" != "No players found" ]
 then
@@ -14,3 +15,4 @@ else
   # Clear any string that was previously displayed
   echo ""
 fi
+# polybar config:2 ends here
