5919c732ecb8b642782d8745fe962eacacb051dc
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-09-01 20:36:03 +0530

    add alias for conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -114,6 +114,7 @@ end
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 # eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# eval /home/shiva/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 ### "vim" as manpager
