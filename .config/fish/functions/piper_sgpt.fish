22f7bb9747e15aaa61ff84b9e376b2319adf2a0d
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-02-20 10:25:11 +0530

    update piper_sgpt func

--- .config/fish/functions/piper_sgpt.fish
+++ .config/fish/functions/piper_sgpt.fish
@@ -1,5 +1,5 @@
 function piper_sgpt
     set prompt $argv
-    set command "sgpt --role chat '$prompt' | ~/gitclones/piper/piper --model ~/gitclones/piper/en_US-hfc_female-medium.onnx  --output-raw | aplay -r 22050 -f S16_LE -t raw -"
+    set command "sgpt --top-p "0.01" --temperature "0.32" --no-cache --role jarvis --repl convi '$prompt' | tee /dev/tty | ~/gitclones/piper/piper --model ~/gitclones/piper/en_US-hfc_female-medium.onnx  --output-raw 2>/dev/null | aplay -r 22050 -f S16_LE -t raw - 2>/dev/null"
     eval $command
 end
